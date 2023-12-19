21226c142ee18458e468ec5006cb04bab985cd6c
Your Name <you@example.com>
2023-12-19 19:11:51 +0500

    git commit -m "this is commit"

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -2,7 +2,16 @@ package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
 
-    public void calculateSum(int number) {
+    public  int calculateSum(int number) {
+        int sum=0;
+        for (int i = number; i !=0  ; i=i/10) {
+            sum=sum+i%10;
+        }
+        return sum;
     }
 
+    public static void main(String[] args) {
+        DigitsSumCalculator digitsSumCalculator=new DigitsSumCalculator();
+        System.out.println(digitsSumCalculator.calculateSum(1234));
+    }
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,22 @@
 package com.epam.langSyntax;
 
-public class NumberReverter {
+import java.util.Scanner;
 
-    public void revert(int number) {
+public class NumberReverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("raqam yozing: ");
+        int berilganRaqam = scanner.nextInt();
+        System.out.println(revert(berilganRaqam));
     }
 
+    public static int revert(int number) {
+        int teskari = 0;
+        for (; number != 0; number /= 10) {
+            int qoldiq = number % 10;
+            teskari = teskari * 10 + qoldiq;
+        }
+        return teskari;
+    }
 }
+
