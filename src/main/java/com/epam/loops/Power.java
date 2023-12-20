0a95fb65b3d4698926ead87185577d5fd1d944c8
Your Name <you@example.com>
2023-12-20 14:10:00 +0500

    git commit -m "this is commit"

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -2,16 +2,13 @@ package com.epam.loops;
 
 public class Power {
 
-    public static int printPower(int numberToPrint, int power) {
-        int print=1;
-        for (int i = 0; i < power; i++) {
-            print *= numberToPrint;
-        }
-        return print;
-    }
+    public  void printPower(int numberToPrint, int power) {
+        long result = 1;
 
-    public static void main(String[] args) {
-//        new Power().printPower(-10, 3);
-        System.out.println(printPower(-10,3));
+        for (int i = 0; i < power; i++) {
+            result *= numberToPrint;
+        }
+
+        System.out.println(numberToPrint + " raised to the power of " + power + " is: " + result);
     }
 }
